f8c074dcc0a465ca116db702f132954ff5e1bba2
Njenduuh <toxiccwillie@gmail.com>
2024-11-07 08:45:20 +0300

    Add UserProfile component with props for user info

--- alx-react-app/src/App.jsx
+++ alx-react-app/src/App.jsx
@@ -2,12 +2,15 @@
 import Header from './components/Header';
 import MainContent from './components/MainContent';
 import Footer from './components/Footer';
+import UserProfile from './components/UserProfile';
+
 
 function App() {
   return (
     <div>
       <Header />
       <MainContent />
+      <UserProfile name="Wilson" age={20} bio="Loves cycling, swimming, and studies German." />
       <Footer />
     </div>
   );
