6d2add915fdec36a5bf970427282802f9578833d
Njenduuh <toxiccwillie@gmail.com>
2024-11-14 00:35:49 +0300

    Finally completed the refactor task

--- alx-react-app-props/src/components/UserProfile.jsx
+++ alx-react-app-props/src/components/UserProfile.jsx
@@ -1,14 +1,16 @@
-// src/components/UserProfile.jsx
+import { useContext } from 'react';
+import UserContext from '../UserContext';
 
-const UserProfile = (props) => {
-    return (
-      <div>
-        <h2>{props.name}</h2>
-        <p>Age: {props.age}</p>
-        <p>Bio: {props.bio}</p>
-      </div>
-    );
-  };
-  
-  export default UserProfile;
-  
+function UserProfile() {
+  const userData = useContext(UserContext); // Accessing user data from context
+
+  return (
+    <div>
+      <h2>Profile</h2>
+      <p>Name: {userData.name}</p>
+      <p>Email: {userData.email}</p>
+    </div>
+  );
+}
+
+export default UserProfile;
