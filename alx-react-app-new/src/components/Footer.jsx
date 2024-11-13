1422d3d378d42173050f723ce1c1f4d4a9eb62a2
Njenduuh <toxiccwillie@gmail.com>
2024-11-13 17:03:04 +0300

    added background,text color to header, styled userprofile,maincontent und footer too

--- alx-react-app-new/src/components/Footer.jsx
+++ alx-react-app-new/src/components/Footer.jsx
@@ -1,10 +1,11 @@
+// src/components/Footer.jsx
+
 function Footer() {
-    return (
-      <footer>
-        <p>© 2023 City Lovers</p>
-      </footer>
-    );
-  }
-  
-  export default Footer;
-  
+  return (
+    <footer style={{ backgroundColor: 'lightgray', color: 'black', textAlign: 'center', padding: '10px', marginTop: '20px' }}>
+      <p>© 2023 City Lovers</p>
+    </footer>
+  );
+}
+
+export default Footer;
--- alx-react-app-new/src/components/UserProfile.jsx
+++ alx-react-app-new/src/components/UserProfile.jsx
@@ -1,14 +1,13 @@
 // src/components/UserProfile.jsx
 
 const UserProfile = (props) => {
-    return (
-      <div>
-        <h2>{props.name}</h2>
-        <p>Age: {props.age}</p>
-        <p>Bio: {props.bio}</p>
-      </div>
-    );
-  };
-  
-  export default UserProfile;
-  
+  return (
+    <div style={{ border: '1px solid gray', padding: '15px', margin: '15px', borderRadius: '5px' }}>
+      <h2 style={{ color: 'blue', fontSize: '1.5em', marginBottom: '10px' }}>{props.name}</h2>
+      <p>Age: <span style={{ fontWeight: 'bold', color: 'darkslategray' }}>{props.age}</span></p>
+      <p>Bio: {props.bio}</p>
+    </div>
+  );
+};
+
+export default UserProfile;
