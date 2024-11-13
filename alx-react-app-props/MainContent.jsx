29e708973ba1a0c0ef3e149df57c0b053d5b9923
Njenduuh <toxiccwillie@gmail.com>
2024-11-14 00:20:09 +0300

    Added Refactor and this time am correct

---
+++ alx-react-app-props/MainContent.jsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import { UserProfile } from './UserProfile';
+
+function MainContent() {
+  return (
+    <main style={{ backgroundColor: '#e0f7fa', padding: '15px', borderRadius: '5px', margin: '10px', textAlign: 'center' }}>
+      <p style={{ fontSize: '18px', color: '#00796b', lineHeight: '1.5', fontWeight: '500' }}>
+        I love to visit New York, Paris, and Tokyo.
+      </p>
+    </main>
+  );
+}
+
+export default MainContent;
+export default UserProfile;
+<<<<<<< HEAD
+
+// ich lerne Deutsch
+=======
+>>>>>>> 491f6920b5eececb4e0edd3c8f55127c4d27e46e
---
+++ alx-react-app-props/UserContext.js
@@ -0,0 +1,7 @@
+// src/UserContext.js
+import React, { createContext } from 'react';
+
+// Create a new context for user data
+const UserContext = createContext();
+
+export default UserContext;
