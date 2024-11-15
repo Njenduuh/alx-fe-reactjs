fe6d0fa676fb1d53fd3abe06d6de4e21b03d325f
Njenduuh <toxiccwillie@gmail.com>
2024-11-15 17:56:26 +0300

    Initial commit for my-company React app

--- my-company/my-company/src/App.jsx
+++ my-company/my-company/src/App.jsx
@@ -1,25 +1,22 @@
-// src/App.jsx
-import React from 'react';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import Home from './Home';       // Import your Home component
-import About from './About';     // Import your About component
-import Services from './Services'; // Import your Services component
-import Contact from './Contact'; // Import your Contact component
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Home from './Home';
+import About from './About';
+import Services from './Services';
+import Contact from './Contact';
+import Navbar from './Navbar';
 
-const App = () => {
+function App() {
   return (
     <Router>
-      <div>
-        {/* Navigation bar would go here (optional) */}
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/about" element={<About />} />
-          <Route path="/services" element={<Services />} />
-          <Route path="/contact" element={<Contact />} />
-        </Routes>
-      </div>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/services" element={<Services />} />
+        <Route path="/contact" element={<Contact />} />
+      </Routes>
     </Router>
   );
-};
+}
 
 export default App;
