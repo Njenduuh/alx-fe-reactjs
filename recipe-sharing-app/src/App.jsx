99d7f5309b1ec15a07231f9361b7e64a948fb642
Njenduuh <toxiccwillie@gmail.com>
2024-11-22 19:07:27 +0300

    I've now fied everything

--- recipe-sharing-app/src/App.jsx
+++ recipe-sharing-app/src/App.jsx
@@ -1,13 +1,25 @@
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import RecipeList from './components/RecipeList';
+import RecipeDetails from './components/RecipeDetails';
 import AddRecipeForm from './components/AddRecipeForm';
 
 function App() {
   return (
-    <div>
-      <h1>Recipe Sharing App</h1>
-      <AddRecipeForm />
-      <RecipeList />
-    </div>
+    <Router>
+      <div>
+        <h1>Recipe Sharing Application</h1>
+        <Routes>
+          {/* Route for the main recipe list */}
+          <Route path="/" element={<RecipeList />} />
+
+          {/* Route for adding a new recipe */}
+          <Route path="/add-recipe" element={<AddRecipeForm />} />
+
+          {/* Route for viewing recipe details */}
+          <Route path="/recipe/:id" element={<RecipeDetails />} />
+        </Routes>
+      </div>
+    </Router>
   );
 }
 
