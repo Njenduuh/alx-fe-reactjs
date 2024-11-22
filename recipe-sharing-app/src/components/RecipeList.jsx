fa3a327e7f201da7b267d4ed4962bc49a3968934
Njenduuh <toxiccwillie@gmail.com>
2024-11-22 19:40:04 +0300

    I'm good at this ngl

--- recipe-sharing-app/src/components/RecipeList.jsx
+++ recipe-sharing-app/src/components/RecipeList.jsx
@@ -1,17 +1,28 @@
-// src/components/RecipeList.jsx
-import { useRecipeStore } from './recipeStore';  // Correct path
+import React from 'react';
+import { Link } from 'react-router-dom'; // Import Link from react-router-dom
+import { useRecipeStore } from '../recipeStore';
 
 const RecipeList = () => {
-  const recipes = useRecipeStore(state => state.recipes);
+  const recipes = useRecipeStore(state => state.filteredRecipes); // Use filteredRecipes from Zustand store
 
   return (
     <div>
-      {recipes.map(recipe => (
-        <div key={recipe.id}>
-          <h3>{recipe.title}</h3>
-          <p>{recipe.description}</p>
-        </div>
-      ))}
+      <h2>Recipe List</h2>
+      {recipes.length === 0 ? (
+        <p>No recipes found</p>
+      ) : (
+        recipes.map(recipe => (
+          <div key={recipe.id}>
+            <h3>
+              {/* Use Link to navigate to the RecipeDetails page */}
+              <Link to={`/recipe/${recipe.id}`}>
+                {recipe.title}
+              </Link>
+            </h3>
+            <p>{recipe.description}</p>
+          </div>
+        ))
+      )}
     </div>
   );
 };
