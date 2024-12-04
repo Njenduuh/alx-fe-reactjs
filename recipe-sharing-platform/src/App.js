490124b996446b4492b2188e000a1524fedaae87
Njenduuh <toxiccwillie@gmail.com>
2024-12-04 15:06:41 +0300

    Router router App.jsx/App.js

--- recipe-sharing-platform/src/App.js
+++ recipe-sharing-platform/src/App.js
@@ -1,13 +1,14 @@
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import HomePage from './components/HomePage';
-import RecipeDetail from './components/RecipeDetail';
+import React from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'; // Importing Router components
+import HomePage from './components/HomePage';  // Import HomePage component
+import RecipeDetail from './components/RecipeDetail'; // Import RecipeDetail component
 
 function App() {
   return (
-    <Router>
-      <Routes>
-        <Route path="/" element={<HomePage />} />
-        <Route path="/recipe/:id" element={<RecipeDetail />} />
+    <Router> {/* Wrap everything inside Router */}
+      <Routes> {/* Define all routes inside Routes */}
+        <Route path="/" element={<HomePage />} /> {/* Home route */}
+        <Route path="/recipe/:id" element={<RecipeDetail />} /> {/* Recipe detail route */}
       </Routes>
     </Router>
   );
