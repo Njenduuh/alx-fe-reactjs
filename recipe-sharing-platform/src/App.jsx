9083c3f2f1c0fbf9ba681fa655a8b0b4a952ab08
Njenduuh <toxiccwillie@gmail.com>
2024-12-04 15:22:26 +0300

    Homepae.jsx containing link, react-router-dom

--- recipe-sharing-platform/src/App.jsx
+++ recipe-sharing-platform/src/App.jsx
@@ -1,13 +1,12 @@
-import React from 'react';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'; // Importing Router components
-import HomePage from './components/HomePage';  // Import HomePage component
-import RecipeDetail from './components/RecipeDetail'; // Import RecipeDetail component
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import HomePage from './components/HomePage';
+import RecipeDetail from './components/RecipeDetail';
 
 function App() {
   return (
-    <Router> {/* Wrap everything inside Router */}
-      <Routes> {/* Define all routes inside Routes */}
-        <Route path="/" element={<HomePage />} /> {/* Home route */}
+    <Router> {/* Wrap everything with Router */}
+      <Routes> {/* Define routes inside Routes */}
+        <Route path="/" element={<HomePage />} /> {/* Home page route */}
         <Route path="/recipe/:id" element={<RecipeDetail />} /> {/* Recipe detail route */}
       </Routes>
     </Router>
