8203ca51f48b488c435be3079dd90ab866e33611
Njenduuh <toxiccwillie@gmail.com>
2024-12-06 23:58:47 +0300

    '

--- recipe-sharing-platform/tailwind.config.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-  content: ['./src/**/*.{js,jsx,ts,tsx}', './public/index.html'],
-  darkMode: false, // Can be set to 'media' or 'class' if needed
-  theme: {
-    extend: {},
-  },
-  plugins: [],
-};
