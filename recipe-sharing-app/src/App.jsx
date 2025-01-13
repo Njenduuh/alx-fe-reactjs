f4019aefbb76c35e467fe95d88364aa9e3595c44
Njenduuh <toxiccwillie@gmail.com>
2025-01-13 11:24:39 +0300

    We gonna astonish the

--- recipe-sharing-app/src/App.jsx
+++ recipe-sharing-app/src/App.jsx
@@ -1,15 +1,24 @@
 import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'; // Importing necessary modules for routing
 import SearchBar from './components/SearchBar';
 import RecipeList from './components/RecipeList';
-import AddRecipeForm from './components/AddRecipeForm'; // Optional
+import RecipeDetails from './components/RecipeDetails'; // Import RecipeDetails for the individual recipe view
 
 const App = () => {
   return (
-    <div className="app">
-      <h1>Recipe Sharing Application</h1>
-      <SearchBar />
-      <RecipeList />
-    </div>
+    <Router>
+      <div className="app">
+        <h1>Recipe Sharing Application</h1>
+        <SearchBar /> {/* Search bar for filtering recipes */}
+        <Routes>
+          {/* Route for displaying the recipe list */}
+          <Route path="/" element={<RecipeList />} />
+
+          {/* Route for displaying individual recipe details based on the recipe ID */}
+          <Route path="/recipe/:id" element={<RecipeDetails />} /> 
+        </Routes>
+      </div>
+    </Router>
   );
 };
 
